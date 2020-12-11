10a6a8a40212fc19054e32dcca66ba38120cc2df
mattedwards <the.matt.edwards@googlemail.com>
2020-12-11 17:30:35 +0000

    adding the rest of day10 files. Failed on part 2. The commented code in .py file was as far as I got

--- day10.py
+++ day10.py
@@ -12,3 +12,15 @@ print(f"Differences of 1: {increm1}")
 print(f"Differences of 3: {increm3 + 1}")
 print(f"Product = {increm1 * (increm3 + 1)}")
 #answer = 2482 (first time success!)
+
+# print(series)
+
+# def find_routes(s, routes=0):
+#     routes += 1
+#     for i in range(len(s)-1):
+#         for j in range(1, 4):
+#             if s[i+j] < s[i] + 4:
+#                 return find_routes(s, routes)
+#     return routes
+
+# print(find_routes(series))
